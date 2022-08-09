c0027313a269533155ac7477b2721569066fe675
martin mithi <gathitumithi9000@gmail.com>
2022-08-09 12:32:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code here!
+const main = document.querySelector("main#main");
+main.remove();
+const newHeader = document.querySelector("h1#victory");
+newHeader.textContent = "Martin is the champion";
